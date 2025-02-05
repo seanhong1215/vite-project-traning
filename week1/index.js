c71b7dbbb9d1becfc7f6ad7259eb158bcb593622
benny <seanhong1215@gmail.com>
2025-02-05 11:40:04 +0800

    1.修改react18 Dom Render寫法
    2.上傳第三週作業

--- week1/index.js
+++ week1/index.js
@@ -120,4 +120,6 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
--- week2/index.js
+++ week2/index.js
@@ -218,4 +218,6 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
