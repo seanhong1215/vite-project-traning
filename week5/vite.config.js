0ead7aca35e7d4b0a121bdbc27156a90bf89a5d9
benny <seanhong1215@gmail.com>
2025-02-06 10:33:41 +0800

    1. 增加專案目錄 2. 新增每週作業資料檔案

--- week5/vite.config.js
+++ week5/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vite-project-traning/week4',
+  base: '/vite-project-traning/week5',
   plugins: [react()],
 })
